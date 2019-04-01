229a31d41c4b3de957897ec44bb988ad8b7c71b2
Gaetan Collaud <gaetan.collaud@spoud.io>
2019-04-01 13:53:01 +0200

    fix http status check

--- backends/http.go
+++ backends/http.go
@@ -223,8 +223,8 @@ func httpRequest(host, uri, username string, withTLS, verifyPeer bool, dataMap m
 		return false
 	}
 
-	if resp.Status != "200" {
-		log.Infof("Wrong http status: %s\n", resp.Status)
+	if resp.StatusCode != 200 {
+		log.Infof("Wrong http status: %v\n", resp.StatusCode)
 		return false
 	}
 
