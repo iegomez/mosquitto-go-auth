4ce413ab81ccc0567d440b206900e95973ed427d
Ignacio Gómez <iegomez@uc.cl>
2019-05-02 09:43:34 -0400

    Add 1.6 support (MOSQ_AUTH_PLUGIN_VERSION 4).

--- auth-plugin.c
+++ auth-plugin.c
@@ -56,7 +56,9 @@ int mosquitto_auth_security_cleanup(void *user_data, struct mosquitto_auth_opt *
   return MOSQ_ERR_SUCCESS;
 }
 
-#if MOSQ_AUTH_PLUGIN_VERSION >=3
+#if MOSQ_AUTH_PLUGIN_VERSION >= 4
+int mosquitto_auth_unpwd_check(void *user_data, struct mosquitto *client, const char *username, const char *password)
+#elif MOSQ_AUTH_PLUGIN_VERSION >=3
 int mosquitto_auth_unpwd_check(void *userdata, const struct mosquitto *client, const char *username, const char *password)
 #else
 int mosquitto_auth_unpwd_check(void *userdata, const char *username, const char *password)
@@ -78,7 +80,9 @@ int mosquitto_auth_unpwd_check(void *userdata, const char *username, const char 
   return MOSQ_ERR_AUTH;
 }
 
-#if MOSQ_AUTH_PLUGIN_VERSION >= 3
+#if MOSQ_AUTH_PLUGIN_VERSION >= 4
+int mosquitto_auth_acl_check(void *user_data, int access, struct mosquitto *client, const struct mosquitto_acl_msg *msg)
+#elif MOSQ_AUTH_PLUGIN_VERSION >= 3
 int mosquitto_auth_acl_check(void *userdata, int access, const struct mosquitto *client, const struct mosquitto_acl_msg *msg)
 #else
 int mosquitto_auth_acl_check(void *userdata, const char *clientid, const char *username, const char *topic, int access)
@@ -107,7 +111,9 @@ int mosquitto_auth_acl_check(void *userdata, const char *clientid, const char *u
   return MOSQ_ERR_ACL_DENIED;
 }
 
-#if MOSQ_AUTH_PLUGIN_VERSION >= 3
+#if MOSQ_AUTH_PLUGIN_VERSION >= 4
+int mosquitto_auth_psk_key_get(void *user_data, struct mosquitto *client, const char *hint, const char *identity, char *key, int max_key_len)
+#elif MOSQ_AUTH_PLUGIN_VERSION >= 3
 int mosquitto_auth_psk_key_get(void *userdata, const struct mosquitto *client, const char *hint, const char *identity, char *key, int max_key_len)
 #else
 int mosquitto_auth_psk_key_get(void *userdata, const char *hint, const char *identity, char *key, int max_key_len)
