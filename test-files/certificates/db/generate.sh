375aa8a3af9d7dbfb5e7ead5ebc32d62c17277f3
Nick Ufer <nick@ufer.dev>
2022-09-09 12:37:50 +0200

    chore: adds second client certificate which acts as unauthorized

--- test-files/certificates/db/generate.sh
+++ test-files/certificates/db/generate.sh
@@ -7,6 +7,7 @@ cfssl genkey -initca ca.json | cfssljson -bare ca
 cfssl sign -ca ../ca.pem -ca-key ../ca-key.pem -config=profiles.json -profile=ca ca.csr | cfssljson -bare ca
 cfssl gencert -ca ca.pem -ca-key ca-key.pem -config=profiles.json -profile=server server.json | cfssljson -bare server
 cfssl gencert -ca ca.pem -ca-key ca-key.pem -config=profiles.json -profile=client client.json | cfssljson -bare client
+cfssl gencert -ca ca.pem -ca-key ca-key.pem -config=profiles.json -profile=client unauthorized-second-client.json | cfssljson -bare unauthorized-second-client
 
 cat server.pem > fullchain-server.pem
 cat ca.pem >> fullchain-server.pem
